1b1b8441e2b3ed1c5000083283b111dca940f935
yassir elyasini <117415601+yssrexe@users.noreply.github.com>
2023-05-18 16:42:15 +0100

    Add files via upload
    commit all files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char str[80] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n",  str);
+	return (1);
+}
