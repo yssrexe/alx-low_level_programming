b184fbd902cf8270b0995086f6b567686b1ba889
elyasiniyassir40@gmail.com <yellex@DESKTOP-7T5IT4H.localdomain>
2023-08-30 21:41:41 +0100

    alx se pro

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,19 +1,14 @@
 #include "main.h"
 /**
- * _puts_recursion - prints a string, followed by a new line
- * @s: string to print
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string
  * Return: nothing
  */
 
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-
-	_putchar(*s);
-	s--;
-	_puts_recursion(s);
+        return;
+	_print_rev_recursion(s + 1);
+    _putchar(*s);
 }
