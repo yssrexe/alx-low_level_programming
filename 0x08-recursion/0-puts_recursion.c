66617077d5bd01b45e04b784e09461bf94df5ee3
elyasiniyassir40@gmail.com <yellex@DESKTOP-7T5IT4H.localdomain>
2023-08-30 20:11:54 +0100

    hello world

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,15 +2,13 @@
 
 void _puts_recursion(char *s)
 {
-    int i = 0;
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
 
-    if(*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
-    
-    _putchar(*s);
-    s++; 
-    _puts_recursion(s);
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
