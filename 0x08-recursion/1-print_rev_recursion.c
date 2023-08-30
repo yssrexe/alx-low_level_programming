d65bb49c7948c3c9ec5a1707abe3afa34ebb70de
elyasiniyassir40@gmail.com <yellex@DESKTOP-7T5IT4H.localdomain>
2023-08-30 22:13:16 +0100

    alx se pro

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,11 +4,10 @@
  * @s: string
  * return: nothing
  */
-
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+    if (*s == '\0')
+        return;
+    _print_rev_recursion(s + 1);
+    _putchar(*s);
 }
