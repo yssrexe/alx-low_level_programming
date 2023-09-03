2a42a308f8ffcec3a45a2b60a7c9dc0f9a99bc12
elyasiniyassir40@gmail.com <yellex@DESKTOP-7T5IT4H.localdomain>
2023-09-03 17:00:53 +0100

    alx se pro

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- * main - name of program
- * @argc: argument count
- * @argv: argument victor
- * Return: return 0 
+ * main - main function
+ * @argc: arg count
+ * @argv: arg victor
+ * Return: return 0
  */
 
 int main(int argc, char **argv)
 {
+	(void) argc;
+
 	printf("%s\n", argv[0]);
-	(void)argc;
 	return (0);
 }
