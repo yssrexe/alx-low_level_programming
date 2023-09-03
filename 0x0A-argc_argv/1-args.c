5da221e43f46dc38188b62928acf9688e19e60b0
elyasiniyassir40@gmail.com <yellex@DESKTOP-7T5IT4H.localdomain>
2023-09-03 22:22:01 +0100

    alx se pro

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
-
+/**
+ * main - program
+ * @argc: argument count
+ * @argv: argument victor
+ * return: always return 0
+*/
 int main(int argc, char **argv)
 {
-    (void)argv;
-     argc = argc - 1;
-    printf("%d\n",argc);
-    return(0);
+	(void)argv;
+	argc = argc - 1;
+	printf("%d\n",argc);
+	return(0);
 }
