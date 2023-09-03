590fcd5df4729a52a315cdfa8ae90270c8f23f4a
elyasiniyassir40@gmail.com <yellex@DESKTOP-7T5IT4H.localdomain>
2023-09-03 15:10:21 +0100

    alx se pro

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,8 @@
-#include<unistd.h>
+#include<stdio.h>
 
 int main(int argc, char **argv)
 {
-    int i = 0;
-
-    if (argc > 1)
-    {
-        while (argv[0][i])
-        {
-            write(1, &argv[0][i], 1);
-            i++;
-        }
-        write(1, "\n", 1);
-    }
+    printf("%s\n", argv[0]);
+    (void)argc;
     return(0);
 }
