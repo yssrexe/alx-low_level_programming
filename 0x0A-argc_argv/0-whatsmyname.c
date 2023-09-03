8ddb775a240630cac794f28716f08436e248c9b0
elyasiniyassir40@gmail.com <yellex@DESKTOP-7T5IT4H.localdomain>
2023-09-03 16:10:38 +0100

    alx se pro

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
  * Return: return 0 
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv)
 {
-        printf("%s\n", argv[0]);
-        (void)argc;
-        return (0);
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
 }
