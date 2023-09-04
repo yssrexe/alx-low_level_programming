d3f131ba93ec3d0a10c14e89c4cad65f2bd34e77
yssrexe <elyasiniyassir40@gmail.com>
2023-09-04 20:17:44 +0100

    alx se pro

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<stdlib.h>
+/**
+ * main - main program
+ * @argc: arg count
+ * @argv: arg victor
+ * return: 0
+ */
+int main(int argc, char **argv)
+{
+	int res;
+
+	if (argc != 3)
+	{
+		printf("stop\n");
+		return (1);
+	}
+	res = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", res);
+	return (0);
+}
