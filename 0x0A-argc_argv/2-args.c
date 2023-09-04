58c5af83e1b46b83b7249c5a156abadd2609131b
yssrexe <elyasiniyassir40@gmail.com>
2023-09-04 19:00:48 +0100

    alx se pro

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
+/**
+ * main - main function
+ * @argc: arg count
+ * @argv: arg victor
+ * return: 0
+*/
 
 int main(int argc, char **argv)
 {
 	(void) argc;
 
-	while(*argv)
+	while (*argv)
 	{
 		printf("%s\n", *argv);
 		argv++;
