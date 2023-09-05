89805dc43c52e2d72afa7e2583890859b5b21b8d
yssrexe <elyasiniyassir40@gmail.com>
2023-09-05 17:34:44 +0100

    alx se pro

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+/**
+ * str_concat - function to str concat ...
+ * @str: string
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i = 0;
+	char *copy;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	copy = malloc(sizeof(char) * (i + 1));
+
+	if (copy == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		copy[i] = str[i};
+		i++;
+	}
+	return (copy);
+}
