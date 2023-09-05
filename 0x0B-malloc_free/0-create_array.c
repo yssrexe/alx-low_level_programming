4a1bb64b81b02d90986743cb7904278732b3e6cb
yssrexe <elyasiniyassir40@gmail.com>
2023-09-05 15:30:12 +0100

    alx se pro

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,16 +10,19 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 
 	p = malloc(sizeof(c) * size);
-	if (!p)
+
+	if (p == NULL)
 		return (NULL);
 
-	while (size-- > 0)
-		*(p + size) = c;
-
-	return (0);
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
 }
