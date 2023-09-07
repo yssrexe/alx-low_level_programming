0e7aa9266fef19125785be4507bce7a40f03bddf
yssrexe <elyasiniyassir40@gmail.com>
2023-09-07 17:28:54 +0100

    alx se pro

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
  * malloc_checked - function that allocates memory
  * @b: unsigned integer
- * Return: always 0
+ * Return: pointer
  */
 
 void *malloc_checked(unsigned int b)
 {
-	int *p;
+	void *p;
 
 	p = malloc(b);
 
