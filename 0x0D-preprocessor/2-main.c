7a3510254b505ac771b2f6b87687e825d7c9818e
yssrexe <elyasiniyassir40@gmail.com>
2023-09-11 17:42:45 +0100

    amx se pro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints the name of the file it was compiled from, followed by a new line
+ * main - program that prints the name of the file it was compiled
  * Return: 0
  */
 
