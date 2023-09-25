68db74f9a1c545dc1d461125dc726461572bd9e9
yssrexe <elyasiniyassir40@gmail.com>
2023-09-25 16:08:21 +0100

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-    int i = 0;
+	int i = 0;
 
-    while (h)
-    {
-        i++;
-        printf("%d\n", h->n);
-        h = h->next;
-    }
-    return (i);
-    
+	while (h)
+	{
+		i++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+
 }
+
