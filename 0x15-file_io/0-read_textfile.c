f8c12dd0db2c6fbc864e27ef61be843ba450187d
yssrexe <elyasiniyassir40@gmail.com>
2023-10-02 14:22:36 +0100

    last update

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	
+}
--- 0x15-file_io/_putchar.c
+++
@@ -1,6 +0,0 @@
-#include "main.h"
-
-char _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
