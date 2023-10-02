2ba45f3ffdfe07b4f97c019a5ff71a42c5cb0620
yssrexe <elyasiniyassir40@gmail.com>
2023-10-02 14:40:37 +0100

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,18 +24,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	
+
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
-	if (o == -1 || r == -1 || w == -1 || w!= r)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free (buffer);
+		free(buffer);
 		return (0);
 	}
 	free(buffer);
 	close(o);
-	
+
 	return (w);
 }
