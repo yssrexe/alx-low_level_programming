68f7e9a4e91c62b7b2ac263f0d22d9068a206c87
yssrexe <elyasiniyassir40@gmail.com>
2024-05-20 14:11:48 +0100

    betty task 00

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,20 +1,27 @@
 #include "search_algos.h"
 
+/**
+ * linear_search - searches for a value in an array
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
+ */
 int linear_search(int *array, size_t size, int value)
 {
-    int i = 0;
+	int i = 0;
 
-    if (!array)
-        return -1;
-    
-    while (i < (int)size)
-    {
-        printf("Value checked array[%d] = [%d]\n",i , array[i]);
-        if (array[i] == value)
-        {
-            return i;
-        }
-        i++;
-    }
-    return -1;
+	if (!array)
+		return (-1);
+
+	while (i < (int)size)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (-1);
 }
